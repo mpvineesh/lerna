44a84bcb0c6b4fad0d0493af0bee755e16ffcbb0
Vineesh M P <vineesh.mavila.pathazhappuray@oracle.com>
2019-09-23 16:47:47 +0530

    init:initial commit

---
+++ ui-snippet/snippet/index.js
@@ -0,0 +1,8 @@
+let str = 'Hello World';
+<<<<<<< HEAD
+let  version = '1.0.1';
+=======
+let  version = '1.0.3';
+>>>>>>> d3bfd85fb89bee663caa7540e4eb310731592d6f
+
+//git log --oneline --pretty=%h;%D;%s;%cd --date=short 
